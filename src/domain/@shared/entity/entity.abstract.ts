9f8b9c574a13aac1c292a183dda1128e2b0c79d1
André Leoni <andreluizleoni@gmail.com>
2022-12-11 13:31:16 -0300

    mvp product validator

--- src/domain/@shared/entity/entity.abstract.ts
+++ src/domain/@shared/entity/entity.abstract.ts
@@ -1,6 +1,8 @@
 import Notification from "../notification/notification";
+
 export default abstract class Entity {
   protected _id: string;
+
   public notification: Notification;
 
   constructor() {
