6165dfdecdc55ddc245a21a30166eb167742c450
André Leoni <andreluizleoni@gmail.com>
2022-12-12 14:02:27 -0300

    improve create and update integration specs

--- src/usecase/product/update/update.product.integration.spec.ts
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -2,9 +2,9 @@ import { Sequelize } from "sequelize-typescript";
 import Product from "../../../domain/product/entity/product";
 import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
 import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
-import CreateProductUseCase from "./create.product.usecase";
+import UpdateProductUseCase from "./update.product.usecase";
 
-describe("Test create product use case", () => {
+describe("Test update product use case", () => {
   let sequelize: Sequelize;
 
   beforeEach(async () => {
@@ -23,22 +23,22 @@ describe("Test create product use case", () => {
     await sequelize.close();
   });
 
-  it("should create a product", async () => {
+  it("should update a product", async () => {
     const productRepository = new ProductRepository();
-    const usecase = new CreateProductUseCase(productRepository);
+    const usecase = new UpdateProductUseCase(productRepository);
 
-    const product = new Product("123", "my product a", 20000);
+    const product = new Product("123", "my product", 20000);
     await productRepository.create(product);
 
     const input = {
-      type: "a",
-      name: "my updated product name",
+      id: "123",
+      name: "my product name",
       price: 30000,
     };
 
     const output = {
       id: expect.any(String),
-      name: "my updated product name",
+      name: "my product name",
       price: 30000,
     };
 
